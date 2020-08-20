95a7ca64cffebd0e6e9240e53a55e9ac3c99c1f7
rakib <jhadul.islam@itconquest.com>
2020-08-20 16:25:42 +0600

    add Base

--- src/main/java/com/rakib/App.java
+++ src/main/java/com/rakib/App.java
@@ -11,15 +11,6 @@ import java.util.logging.Logger;
 public class App {
     public static void main(String[] args) {
 
-        System.setProperty("hadoop.home.dir", "c:/hadoop");
-        Logger.getLogger("org.apache").setLevel(Level.WARNING);
 
-        SparkSession session = SparkSession.builder().appName("SparkSQL").master("local[*]")
-                .config("spark.sql.warehouse.dir", "file:///c:/temp/")
-                .getOrCreate();
-
-
-
-        session.close();
     }
 }
