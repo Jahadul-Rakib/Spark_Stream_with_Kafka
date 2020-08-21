2961b3927d198ddf4669a1323245f24f2d19c885
rakib <jhadul.islam@itconquest.com>
2020-08-21 12:43:55 +0600

    add

--- src/main/java/com/rakib/App.java
+++ src/main/java/com/rakib/App.java
@@ -7,8 +7,6 @@ import org.apache.spark.sql.streaming.OutputMode;
 import org.apache.spark.sql.streaming.StreamingQuery;
 import org.apache.spark.sql.streaming.StreamingQueryException;
 
-import java.util.HashMap;
-import java.util.Map;
 import java.util.concurrent.TimeoutException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -16,25 +14,29 @@ import java.util.logging.Logger;
 public class App {
     public static void main(String[] args) throws TimeoutException, StreamingQueryException {
 
-        System.setProperty("hadoop.home.dir","c:/hadoop");
+        System.setProperty("hadoop.home.dir", "c:/hadoop");
         Logger.getLogger("org.apache").setLevel(Level.WARNING);
 
-        SparkSession sparkSession = SparkSession.builder().appName("SparkSQL").master("local[*]")
-                .config("spark.sql.warehouse.dir", "file:///c:/temp/")
+        SparkSession sparkSession = SparkSession.builder()
+                .appName("SparkSQL")
+                .master("local[*]")
                 .getOrCreate();
 
-        Dataset<Row> rowDataset = sparkSession.readStream()
+        Dataset<Row> rowDataset = sparkSession
+                .readStream()
                 .format("kafka")
                 .option("kafka.bootstrap.servers", "localhost:9091,localhost:9092,localhost:9093")
-                .option("subscribe", "student")
+                .option("subscribe", "studentinfo")
+                .option("startingOffsets", "earliest")
                 .load();
-
-        rowDataset.createOrReplaceTempView("student_info");
-
-        Dataset<Row> dataset = sparkSession.sql("SELECT CAST(value AS STRING) FROM student_info");
+        rowDataset.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)");
 
 
-        StreamingQuery query = dataset
+        //rowDataset.createOrReplaceTempView("student_info");
+        //Dataset<Row> dataset = sparkSession.sql("SELECT value FROM student_info");
+
+
+        StreamingQuery query = rowDataset
                 .writeStream()
                 .format("console")
                 .outputMode(OutputMode.Append())
